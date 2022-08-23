f7195ff8c6fc757e2f1f82d702d2e9ac0c02852e
Damian <damianna070@gmail.com>
2022-08-23 11:04:45 -0600

    Initial Commit

---
+++ data types/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  data types
+//
+//  Created by Sosa Espada, Damian - Student on 8/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var intigerType:Int=5
+
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ok/ok/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ok
+//
+//  Created by Sosa Espada, Damian - Student on 8/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
