68e5840625b60ea0b744c1cf23fa82e9bc60fd23
Damian <damianna070@gmail.com>
2022-10-13 09:40:10 -0600

    About Me Project

--- final calc/final calc/ViewController.swift
+++ final calc/final calc/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var labelAnswer: UILabel!
+    
+    
+    @IBOutlet weak var textbox1: UITextField!
+    var textbox1String=""
+    var textbox11Double=0.0
+    
+    
+    @IBOutlet weak var textbox2: UITextField!
+    var textbox2String=""
+    var textbox22Double=0.0
+    
+    
+    @IBOutlet weak var textbox3: UITextField!
+    var textbox3String=""
+    var textbox33Double=0.0
+    
+    var anserDouble = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func button(_ sender: Any) {
+        
+        
+        
+    }
+    
 
 }
 
