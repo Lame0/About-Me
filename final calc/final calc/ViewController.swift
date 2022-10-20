c041eccc38312477e8d2125937a3841a1587e574
Damian <damianna070@gmail.com>
2022-10-20 10:46:49 -0600

    map

--- final calc/final calc/ViewController.swift
+++ final calc/final calc/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var labelAnswer: UILabel!
     
     
+    
     @IBOutlet weak var textbox1: UITextField!
     var textbox1String=""
     var textbox11Double=0.0
@@ -34,8 +35,30 @@ class ViewController: UIViewController {
     }
     @IBAction func button(_ sender: Any) {
         
+        textbox1String = textbox1.text ?? ""
+        textbox2String = textbox2.text ?? ""
+        textbox3String =   textbox3.text ?? ""
+        print(textbox3String)
+        textbox11Double = Double(textbox1String) ?? 0.0
+        textbox22Double = Double(textbox2String) ?? 0.0
+        textbox33Double = Double(textbox3String) ?? 0.0
+        print(textbox33Double)
+        var pie = 100-textbox33Double
+        print(pie)
+        var currentgradeweight=textbox11Double*pie/100
+        print(currentgradeweight)
+        var
+        desiredgrade=textbox22Double-currentgradeweight
+        print(desiredgrade)
+        var
+        examscore=desiredgrade/textbox33Double*100
         
         
+        var roundedValue = round(examscore
+                            * 10) / 10.0
+        // roundedValue uses the round function
+        
+        labelAnswer.text="\(roundedValue)"
     }
     
 
